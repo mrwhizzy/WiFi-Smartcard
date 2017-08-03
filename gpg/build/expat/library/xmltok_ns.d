d3e41af71676244a47a45329153bff8c99c9af68
Christos Melas <cxm661@cs.bham.ac.uk>
2017-08-03 15:20:35 +0100

    made clean

--- gpg/build/expat/library/xmltok_ns.d
+++
@@ -1,2 +0,0 @@
-library/xmltok_ns.o: \
- /Users/CMelas/esp/esp-idf/components/expat/library/xmltok_ns.c
--- gpg/build/jsmn/src/jsmn.d
+++
@@ -1,4 +0,0 @@
-src//jsmn.o: /Users/CMelas/esp/esp-idf/components/jsmn/src//jsmn.c \
- /Users/CMelas/esp/esp-idf/components/jsmn/include/jsmn.h
-
-/Users/CMelas/esp/esp-idf/components/jsmn/include/jsmn.h:
