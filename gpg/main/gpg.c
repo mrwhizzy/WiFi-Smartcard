9dd8b482fe5c906d52356c7bccb89db4537a627e
Christos Melas <cxm661@cs.bham.ac.uk>
2017-08-05 00:43:09 +0100

    Fixed minor bug. Was just never closing the sockets after ending connections.

--- gpg/main/gpg.c
+++ gpg/main/gpg.c
@@ -30,6 +30,7 @@
 
 #define PORT 5511       // The default port of this protocol
 #define PRINTAPDU       // If defined, APDU info is printed
+//#define PROCEEDBTN    // Do not connect until the proceed button is pressed
 
 // FreeRTOS event group to signal when we are connected & ready to make a request
 static EventGroupHandle_t wifi_event_group;
@@ -49,11 +50,11 @@ uint8_t connected = 0;
 uint8_t proceed = 0;    // When the proceed button is pressed, proceed is set
 uint8_t hardRst = 0;    // When the hard reset button is pressed, hardRst is set
 
-void proceedHandle(void* arg) {
+void proceedHandle(void* arg) {     // Interrupt handler for the proceed button
     proceed = 1;
 }
 
-void hardReset(void* arg) {
+void hardReset(void* arg) {         // Interrupt handler for the hard reset button
     hardRst = 1;
 }
 
@@ -95,7 +96,7 @@ void initGPIO() {
     gpio_isr_handler_add(GPIO_NUM_17, hardReset, (void*) GPIO_NUM_17);      // Hook ISR handler
 }
 
-void initNVS() {
+void initNVS() {        // Initialize the non-volatile storage
     esp_err_t err = nvs_flash_init();
     if (err == ESP_ERR_NVS_NO_FREE_PAGES) {
         // NVS partition was truncated and needs to be erased
@@ -110,7 +111,7 @@ void initNVS() {
 }
 
 static esp_err_t event_handler(void *ctx, system_event_t *event) {
-    static const char *TAG = "event_handler";
+    static const char *TAG = "wifiEventHandler";
 
     switch(event->event_id) {
     case SYSTEM_EVENT_STA_START:
@@ -141,10 +142,10 @@ static esp_err_t event_handler(void *ctx, system_event_t *event) {
     return ESP_OK;
 }
 
-static void initWiFi(void) {
+static void initWiFi(void) {    // Configure and initialize Wi-Fi
     nextNet = 0;        // Attempt to connect to this network next
 
-    tcpip_adapter_init();
+    tcpip_adapter_init();   // Initialize the TCP/IP adapter
     wifi_event_group = xEventGroupCreate();
     ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
 
@@ -168,21 +169,21 @@ static void taskConnect(void *pvParameters) {
     nvs_handle nvsHandle;       // Open NVS to check if the device has been initialized
     uint8_t initialized = 0;    // Flag to check
     gpio_set_level(GPIO_NUM_25, 1);     // Initialize/restore start
-    esp_err_t err = nvs_open("storage", NVS_READWRITE, &nvsHandle);
+    esp_err_t err = nvs_open("storage", NVS_READWRITE, &nvsHandle); // Open the NVS
     if (err != ESP_OK) {
         goto exit;
     } else {
-        err = nvs_get_u8(nvsHandle, "initialized", &initialized);
+        err = nvs_get_u8(nvsHandle, "initialized", &initialized);   // Get the initialized value
         nvs_close(nvsHandle);
         switch (err) {
             case ESP_OK:        // If it has already been initialized, restore the data
-                if (restoreState() != 0) {
+                if (restoreState() != 0) {  // If something went wrong, then restart
                     hardRst = 1;
                     goto exit;
                 }
                 break;
             case ESP_ERR_NVS_NOT_FOUND:     // If not initialized, then initialize
-                if (initialize() != 0) {
+                if (initialize() != 0) {    // If something went wrong, then restart
                     goto exit;
                 }
                 break;
@@ -195,74 +196,79 @@ static void taskConnect(void *pvParameters) {
     // Wait for the callback to set the CONNECTED_BIT in the event group.
     xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);
     ESP_LOGI(TAG, "Connected to AP");
+
+#ifdef PROCEEDBTN
     printf("\nPress the proceed button to connect to: %s\n", IP[currNet]);
     fflush(stdout);
     while (proceed == 0) {
         vTaskDelay(1000/portTICK_PERIOD_MS);
     }
     proceed = 0;
+#endif
 
     while(1) {
+begin:
         serv_addr.sin_family = AF_INET;
         serv_addr.sin_port = htons(PORT);
         serv_addr.sin_addr.s_addr = inet_addr(IP[currNet]);
-
         sockfd = socket(AF_INET, SOCK_STREAM, 0);
         if (sockfd < 0) {
-            ESP_LOGE(TAG, "... Failed to allocate socket");
+            ESP_LOGE(TAG, "... Failed to allocate socket: %d", errno);
             vTaskDelay(1000/portTICK_PERIOD_MS);
-            continue;
+            goto exit;
         }
         ESP_LOGI(TAG, "... allocated socket");
 
         if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0) {
             ESP_LOGE(TAG, "... socket connect failed errno: %d", errno);
+            ESP_LOGI(TAG, "Check that the server is running on the other end");
             close(sockfd);
-            vTaskDelay(1000/portTICK_PERIOD_MS);
-            goto exit;
+            vTaskDelay(10000/portTICK_PERIOD_MS);
+            ESP_LOGI(TAG, "Trying again ...\n");
+            goto begin;
         }
         ESP_LOGI(TAG, "... connected\n");
 
-        while(1) {
-            bzero(recvBuf, sizeof(recvBuf));                // Zero the receive buffer
-            r = read(sockfd, recvBuf, sizeof(recvBuf)-1);   // Receive the APDU command
-            comAPDU = parseAPDU(recvBuf, r);                // Parse the APDE command
+        bzero(recvBuf, sizeof(recvBuf));                // Zero the receive buffer
+        r = read(sockfd, recvBuf, sizeof(recvBuf)-1);   // Receive the APDU command
+        comAPDU = parseAPDU(recvBuf, r);                // Parse the APDE command
 
-            if (comAPDU.INS == 0x00) {          // Nothing more to receive
-                break;
-            }
-
-#ifdef PRINTAPDU
-            printf("CLA: %02X\tINS: %02X\tP1: %02X\t", comAPDU.CLA, comAPDU.INS, comAPDU.P1);
-            printf("P2: %02X\tP1P2: %02X\tLc: %02X\tData: ", comAPDU.P2, comAPDU.P1P2, comAPDU.Lc);
-            const uint8_t* tmp = comAPDU.data;
-            while(*tmp)
-                printf("%02X ", (unsigned int) *tmp++);
-            printf("\nLe: %02X\tTotal: %d\n", comAPDU.Le, r);
-            fflush(stdout);
-#endif
-
-            gpio_set_level(GPIO_NUM_25, 1);     // Start processing a command
-            process(comAPDU, &output);          // Perform the appropriate operation
-            gpio_set_level(GPIO_NUM_25, 0);     // End of command processing
-
-#ifdef PRINTAPDU
-            printf("Output Data: ");
-            const uint8_t* tmp2 = output.data;
-            while(*tmp2)
-                printf("%02X ", (unsigned int) *tmp2++);
-            printf("\nLength: %d\n", output.length);
-            fflush(stdout);
-#endif
-
-            if (write(sockfd, output.data, output.length) < 0) {    // Send the response
-                ESP_LOGE(TAG, "... socket send failed");
-                close(sockfd);
-                vTaskDelay(1000/portTICK_PERIOD_MS);
-                goto exit;
-            }
-            ESP_LOGI(TAG, "... socket send success\n");
+        if (comAPDU.INS == 0x00) {      // Nothing more to receive
+            close(sockfd);
+            goto begin;
         }
+
+#ifdef PRINTAPDU
+        printf("CLA: %02X\tINS: %02X\tP1: %02X\t", comAPDU.CLA, comAPDU.INS, comAPDU.P1);
+        printf("P2: %02X\tP1P2: %02X\tLc: %02X\tData: ", comAPDU.P2, comAPDU.P1P2, comAPDU.Lc);
+        const uint8_t* tmp = comAPDU.data;
+        while(*tmp)
+            printf("%02X ", (unsigned int) *tmp++);
+        printf("\nLe: %02X\tTotal: %d\n", comAPDU.Le, r);
+        fflush(stdout);
+#endif
+
+        gpio_set_level(GPIO_NUM_25, 1);     // Start processing a command
+        process(comAPDU, &output);          // Perform the appropriate operation
+        gpio_set_level(GPIO_NUM_25, 0);     // End of command processing
+
+#ifdef PRINTAPDU
+        printf("Output Data: ");
+        const uint8_t* tmp2 = output.data;
+        while(*tmp2)
+            printf("%02X ", (unsigned int) *tmp2++);
+        printf("\nLength: %d\n", output.length);
+        fflush(stdout);
+#endif
+
+        if (write(sockfd, output.data, output.length) < 0) {    // Send the response
+            ESP_LOGE(TAG, "... socket send failed");
+            vTaskDelay(1000/portTICK_PERIOD_MS);
+            close(sockfd);
+            goto exit;
+        }
+        ESP_LOGI(TAG, "... socket send success\n");
+        close(sockfd);
     }
 
 exit:
@@ -276,8 +282,8 @@ exit:
 }
 
 static void checkReset(void *pvParameters) {
-    while(1) {
-        if (hardRst == 1) {          // Erase "initialized" from NVS and restart
+    while(1) {      // Check if reset button is pressed periodically by polling
+        if (hardRst == 1) {     // If it was pressed, erase "initialized" from NVS
             nvs_handle nvsHandle;
             if (nvs_open("storage", NVS_READWRITE, &nvsHandle) == ESP_OK) {
                 if (nvs_erase_key(nvsHandle, "initialized") == ESP_OK) {
@@ -292,9 +298,9 @@ static void checkReset(void *pvParameters) {
 
 static void wifiStatus(void *pvParameters) {
     uint8_t toggle = 0;     // Toggle for Wi-Fi status LED
-    while(1) {
+    while(1) {  // Flash while it is still looking for a known network
         if (connected == 0) {
-            toggle ^= 1;
+            toggle ^= 1;    // Because using XOR to toggle 0 and 1 is cool
             gpio_set_level(GPIO_NUM_26, toggle);
         }
         vTaskDelay(500/portTICK_PERIOD_MS);
