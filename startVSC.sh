7134f67b02cd94faecd767be68fe859b632c3ffe
Christos Melas <cxm661@student.bham.ac.uk>
2017-08-07 02:22:51 +0100

    Minor changes and tests

--- startVSC.sh
+++ startVSC.sh
@@ -4,12 +4,12 @@ do
 	if [ ! `pgrep pcscd` ] ; then
 		/usr/sbin/pcscd
 		if [ ! `pgrep pcscd` ] ; then
-			echo "Make sure you run this script with root privileges"
+			echo "Make sure you are running this script as root"
 			exit
 		fi
 	fi
 	if [ ! `pgrep vicc` ] ; then
-		/usr/local/bin/vicc --esp32 &
+		/usr/local/bin/vicc --esp32
 	fi
 	sleep 2
 done
