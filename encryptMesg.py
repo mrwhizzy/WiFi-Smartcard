496c75b48d8de126c3cdd09c71550118684ef673
Christos Melas <cxm661@student.bham.ac.uk>
2017-08-19 23:33:52 +0100

    Minor change, input can be enterd from the keyboard now

--- encryptMesg.py
+++ encryptMesg.py
@@ -8,10 +8,13 @@ f = open("private.pem", 'r')
 key = RSA.importKey(f.read(), passphrase = "12345678")
 f.close()
 
-message = "Foo, bar baz."
+while True:
+	inp = raw_input("Input (max. 245 characters): ")
+	if (len(inp) <= 245):
+	    break   
 
 cipher = PKCS1_v1_5.new(key)
-ciphertext = cipher.encrypt(message)
+ciphertext = cipher.encrypt(inp)
 
 f = open('ciphertext', 'wb')
 f.write(ciphertext)
